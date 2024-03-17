ba9c3d27c04bcbc88f1e325c6fb19bbe88e882dc
Izumi <dummyacczwei@gmail.com>
2024-03-17 20:19:58 +0800

    updated

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] random_numbers = new int[5];
-
-        random_numbers[0] = 14;
-        random_numbers[1] = 18;
-        random_numbers[2] = 36;
-        random_numbers[3] = 43;
-        random_numbers[4] = 45;
-
-        Arrays.sort(random_numbers);
+        int[] random_numbers = {14, 18, 36, 43, 45};
 
         int sumEven = 0;
         for (int num : random_numbers) {
